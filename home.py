6aaa44661a5c548cc5c7c1dd1e6be18702b73a3b
Vardhan <es22btech11031@iith.ac.in>
2025-05-09 00:24:23 +0530

    Minor changes

--- home.py
+++ home.py
@@ -11,26 +11,26 @@ if not st.session_state["view_grades_exists"]:
         fp.write(view_grades_contents)
     st.session_state["view_grades_exists"] = True
 
-st.set_page_config("View Your Grades")
-st.title("View Your Grades")
+st.set_page_config("View my Grades")
+st.title("View my Grades")
 
 grade_viewer_tab, help_tab = st.tabs(["Grade Viewer", "Help"])
 
 with grade_viewer_tab:
-    cookie = grade_viewer_tab.text_input("Your `JSESSIONID` cookie", type="password")
+    cookie = grade_viewer_tab.text_input("`JSESSIONID` cookie", type="password")
 
     if cookie:
         from pages.runtime.view_grades import view_grades
         view_grades(cookie, grade_viewer_tab)
 
 with help_tab:
-    help_tab.subheader("Obtaining Your Cookie")
+    help_tab.subheader("Obtaining the Cookie")
     help_tab.markdown("""
                     1. Log in to [the AIMS website](https://aims.iith.ac.in/aims/).
 
-                    2. Follow the steps in the image below to obtain your `JSESSIONID` cookie.
+                    2. Follow the steps in the image below to obtain the `JSESSIONID` cookie.
 
-                    3. Make sure that your cookie doesn't expire (don't log out, nor get logged out of the AIMS website).
+                    3. Make sure that the cookie doesn't expire (don't log out, nor get logged out of the AIMS website).
                     """
                 )
-    help_tab.image("./images/JSESSIONID_cookie.png", "Obtaining your `JSESSIONID` cookie.")
+    help_tab.image("./images/JSESSIONID_cookie.png", "Obtaining the `JSESSIONID` cookie.")
